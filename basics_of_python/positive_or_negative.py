ca062e1674f7cfb5eb8718d9d0ecf74c95c6d598
vanshika <vanshikachoudhary07003@gmail.com>
2025-05-30 23:10:03 +0530

    moving folder to git

---
+++ basics_of_python/positive_or_negative.py
@@ -0,0 +1,12 @@
+num= int(input("enter a no.= "))
+
+if (num>0):
+    print("This no. is a positive number")
+
+elif(num<0):
+    print("This no, is negative number")    
+
+else:
+    print("This no. is zero")    
+
+
