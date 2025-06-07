d401a084b0436848af33cd0544d84e6ee40cde7c
Vanshika <vanshikachoudhary07003@gmail.com>
2025-06-07 20:36:05 +0530

    Update hollow_square_pattern.py

--- patterns/hollow_square_pattern.py
+++ patterns/hollow_square_pattern.py
@@ -1,1 +1,9 @@
+rows=int(input("Enter hollow square star pattern rows="))
 
+for i in range(rows):
+    for j in range(rows):
+         if(i==0 or i==rows-1 or j==0 or j==rows-1):
+            print("*",end='')
+         else:
+            print(" ", end='')   
+    print()         
